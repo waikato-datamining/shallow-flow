9cd1e535180255d051f15a23d306301175fd947b
Peter Reutemann <fracpete@gmail.com>
2021-08-22 11:23:05 +1200

    added API for storage handling

--- base/src/shallowflow/base/controls/_Flow.py
+++ base/src/shallowflow/base/controls/_Flow.py
@@ -1,11 +1,19 @@
 from shallowflow.api.control import ActorHandler
+from shallowflow.api.storage import StorageHandler, Storage
 from shallowflow.base.directors import SequentialDirector
 
 
-class Flow(ActorHandler):
+class Flow(ActorHandler, StorageHandler):
     """
     Encapsulates a complete flow.
     """
+
+    def _initialize(self):
+        """
+        Initializes the members.
+        """
+        super()._initialize()
+        self._storage = Storage()
 
     def _new_director(self):
         """
@@ -15,3 +23,13 @@ class Flow(ActorHandler):
         :rtype: AbstractDirector
         """
         return SequentialDirector(owner=self, requires_source=True, requires_sink=False)
+
+    @property
+    def storage(self):
+        """
+        Returns the storage.
+
+        :return: the storage
+        :rtype: Storage
+        """
+        return self._storage
