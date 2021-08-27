8c09d45c52f47d7487d7c77e29d075a50f666675
Peter Reutemann <fracpete@gmail.com>
2021-08-27 14:42:42 +1200

    using Sequence actor now

--- examples/branching.py
+++ examples/branching.py
@@ -1,7 +1,8 @@
 from scoping import scoping
-from shallowflow.base.controls import Flow, Branch, run_flow
+from shallowflow.base.controls import Flow, Branch, Sequence, run_flow
 from shallowflow.base.sinks import ConsoleOutput
 from shallowflow.base.sources import ForLoop
+from shallowflow.base.transformers import PassThrough
 
 forloop = ForLoop()
 
@@ -9,9 +10,12 @@ branch = Branch()
 # generates five branches with different prefixes for the console output
 for i in range(5):
     with scoping():
+        seq = Sequence()
+        pt = PassThrough()  # added for the sequence to make sense :-)
         output = ConsoleOutput() \
             .set("prefix", "branch-" + str(len(branch.actors) + 1) + ": ")
-        branch.append(output)
+        seq.actors = [pt, output]
+        branch.append(seq)
 
 flow = Flow()
 flow.actors = [forloop, branch]
